4d7c42c09793187013e464d3f77fc05fcbc9c713
19025782-DevinLianDiLin <68309540+19025782-DevinLianDiLin@users.noreply.github.com>
2021-06-02 13:25:57 +0800

    Delete Item information
    Was used as a temporary measure to store data

--- ItemInformation.py
+++
@@ -1,8 +0,0 @@
-class Item:
-    def __init__(self, item_id, item_locker_size, item_description):
-        self.id = item_id
-        self.ls = item_locker_size
-        self.des = item_description
-
-
-# item stored is to check if item is already used by the locker
